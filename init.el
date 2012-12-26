901ea795206ee4cd3999fdee1ba111df93c9d483
Titan Tang <titantang@gmail.com>
2012-12-26 12:41:40 +0800

    chinese display settings

--- init.el
+++ init.el
@@ -29,6 +29,37 @@
 (setq-default whitespace-style '(face trailing tabs))
 (global-whitespace-mode 1)
 
+;; chinese display
+(when (eq window-system 'ns)
+  (let ((my-font-height 120)
+        (my-font (cond
+                  (t   "Monaco")  ;; XCode 3.1
+                  (nil "Menlo")   ;; XCode 3.2
+                  ))
+        (my-font-ja "STHeiti"))
+
+    (setq mac-allow-anti-aliasing t)
+    (setq face-font-rescale-alist
+          '(("^-apple-hiragino.*" . 1.2)
+            (".*osaka-bold.*" . 1.2)
+            (".*osaka-medium.*" . 1.2)
+            (".*courier-bold-.*-mac-roman" . 1.0)
+            (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+            (".*monaco-bold-.*-mac-roman" . 0.9)
+            ("-cdac$" . 1.3)))
+
+    (when my-font
+      (set-face-attribute 'default nil :family my-font :height my-font-height)
+      ;;(set-frame-font (format "%s-%d" my-font (/ my-font-height 10)))
+      )
+
+    (when my-font-ja
+      (let ((fn (frame-parameter nil 'font))
+            (rg "iso10646-1"))
+        (set-fontset-font fn 'chinese-gb2312 `(,my-font-ja . ,rg))
+        (set-fontset-font fn 'chinese-gbk `(,my-font-ja . ,rg))))))
+;;        (set-fontset-font fn 'unicode `(,my-font-ja . ,rg))))))
+
 (require 'autopair)
 (autopair-global-mode) ;; enable autopair in all buffers
 
