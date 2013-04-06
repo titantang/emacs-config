9e3f582a7385508987b3db0c62f505e68fe41a27
Titan Tang <titantang@gmail.com>
2013-04-06 16:22:25 +0800

    upgrade plugins

---
+++ elpa/php-extras-0.4.0.20130402/php-extras-pkg.el
@@ -0,0 +1,3 @@
+(define-package "php-extras" "0.4.0.20130402"
+  "Extra features for `php-mode'"
+  '((php-mode "1.5.0")))
---
+++ elpa/ruby-test-mode-1.5/ruby-test-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ruby-test-mode" "1.5" "Minor mode for Behaviour and Test Driven" (quote ((ruby-mode "1.0"))))
