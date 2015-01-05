16a4788e668921a618c7fa5ac3f4ded2334aa42d
Titan Tang <titantang@gmail.com>
2015-01-05 08:41:53 +0800

    update plugins

---
+++ elpa/gist-20141230.158/gist-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gist" "20141230.158" "Emacs integration for gist.github.com" (quote ((eieio "1.4") (gh "0.8.1") (tabulated-list "0"))))
---
+++ elpa/rbenv-20141119.2349/rbenv-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rbenv" "20141119.2349" "Emacs integration for rbenv" (quote nil))
